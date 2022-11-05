5e01af564da9eaf6d8a2db254986cf33f1098d07
viviannemelo <viviannemmelo@gmail.com>
2022-11-05 10:49:24 -0300

    Refazendo Requisito 02 (erro não identificado)

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,71 +1,84 @@
-import { isDisabled } from '@testing-library/user-event/dist/utils';
 import React, { Component } from 'react';
-// import { Redirect } from 'react-router-dom';
-// import PropTypes from 'prop-types';
-// import Loading from '../components/Loading';
-// import { createUser } from '../services/userAPI';
+import PropTypes from 'prop-types';
+import Loading from '../components/Loading';
+import { createUser } from '../services/userAPI';
 
 class Login extends Component {
   state = {
-    isDisabled: false,
+    isButtonDisabled: true,
     nameLogin: '',
+    loading: true,
   };
 
-  handleChange = ({ target }) => {
+  handleChange = (event) => {
+    const { name } = event.target;
+    const { value } = event.target;
     this.setState({
-      [target.name]: target.value,
+      [name]: value,
+    }, this.enabledButton);
+  };
+
+  enabledButton = () => {
+    const { nameLogin } = this.state;
+    const minLength = 3;
+    const nameIsValid = nameLogin.length >= minLength;
+    const disabled = nameIsValid;
+    this.setState({
+      isButtonDisabled: disabled,
     });
   };
 
-  // handleChangeInputName = (event) => {
-  //   const { name } = event.target;
-  //   const { value } = event.target;
-  //   this.setState({
-  //     [name]: value,
-  //   }, (() => {
-  //     const minLength = 3;
-  //     const nameLength = nameLogin.length > minLength;
-  //     const verifyName = nameLength;
-  //     this.setState({
-  //       isDisabled: verifyName,
-  //     });
-  //   }
-  //   ));
-  // };
+  handleClick = async () => {
+    const { nameLogin } = this.state;
+    const { history } = this.props;
+    this.setState({
+      loading: false,
+    });
+    await createUser({ name: nameLogin });
+    history.push('/search');
+  };
 
   render() {
-    const { nameLogin, isDisabled } = this.state;
+    const { nameLogin, isButtonDisabled, loading } = this.state;
 
     return (
       <div data-testid="page-login">
-        <section className="login-name-input">
-          <label htmlFor="name-login">
-            <input
-              data-testid="login-name-input"
-              type="text"
-              id="login-name-input"
-              name="nameLogin"
-              value={ nameLogin }
-              onChange={ this.handleChange }
-            />
-          </label>
-          <button
-            data-testid="login-submit-button"
-            type="submit"
-            disabled={ isDisabled }
-            onClick={ this.fetchUserApi }
-          >
-            Entrar
-          </button>
-        </section>
+        {
+          loading ? <Loading /> : (
+            <section className="login-section">
+              <label htmlFor="login-name-input">
+                <input
+                  data-testid="login-name-input"
+                  type="text"
+                  id="login-name-input"
+                  name="nameLogin"
+                  placeholder="Nome"
+                  value={ nameLogin }
+                  onChange={ this.handleChange }
+                />
+              </label>
+              <button
+                data-testid="login-submit-button"
+                type="submit"
+                disabled={ isButtonDisabled }
+                onClick={ this.handleClick }
+              >
+                Entrar
+              </button>
+            </section>
+          )
+        }
       </div>
     );
   }
 }
 
-// Login.propTypes = {
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
 //   nameLogin: PropTypes.string.isRequired,
 //   buttonEnabled: PropTypes.bool.isRequired,
-// };
+};
 
 export default Login;
